147ef52125e9a620d9bd9a171c569855b3788840
Patrick Boos <pboos@carecon.com>
2018-12-06 22:41:14 +0100

    Version 3.1.1-SNAPSHOT

--- kernel/api/fabric3-host-api/src/main/java/org/fabric3/api/host/Names.java
+++ kernel/api/fabric3-host-api/src/main/java/org/fabric3/api/host/Names.java
@@ -26,7 +26,7 @@ import java.net.URI;
  */
 public interface Names {
 
-    String VERSION = "3.1.0";
+    String VERSION = "3.1.1-SNAPSHOT";
 
     URI BOOT_CONTRIBUTION = URI.create("fabric3-boot");
 
